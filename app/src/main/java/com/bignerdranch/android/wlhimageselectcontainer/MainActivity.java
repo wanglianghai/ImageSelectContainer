2d78cfd787453340ab5b954efb6be13a1a2c26d9
wanglianghai <1540446221@qq.com>
2017-07-10 10:29:02 +0800

    进程间的通信获取图片的地址

--- app/src/main/java/com/bignerdranch/android/wlhimageselectcontainer/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/wlhimageselectcontainer/MainActivity.java
@@ -1,13 +1,26 @@
 package com.bignerdranch.android.wlhimageselectcontainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSelectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSelectButton = (Button) findViewById(R.id.select_picture);
+        mSelectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this 表示对当前对象的引用
+                startActivity(new Intent(MainActivity.this , ImageSelectActivity.class));
+            }
+        });
     }
 }
