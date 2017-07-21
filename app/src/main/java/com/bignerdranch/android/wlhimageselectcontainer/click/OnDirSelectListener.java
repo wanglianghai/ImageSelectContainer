23f66095d9d60aace265ae0ae126cafa6588c4b7
wanglianghai <1540446221@qq.com>
2017-07-21 16:38:04 +0800

    漏了一个

---
+++ app/src/main/java/com/bignerdranch/android/wlhimageselectcontainer/click/OnDirSelectListener.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.wlhimageselectcontainer.click;
+
+import com.bignerdranch.android.wlhimageselectcontainer.bean.ImageDirBean;
+
+/**
+ * Created by Administrator on 2017/7/21/021.
+ */
+
+public interface OnDirSelectListener {
+    void selectDirPath(ImageDirBean bean);
+}
